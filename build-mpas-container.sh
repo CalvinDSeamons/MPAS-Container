073c5235862777a875defb1f70b5958f6a946510
Calvin Seamons <calvindseamons@lanl.gov>
2019-11-25 11:46:07 -0700

    minor updates to build

--- build-mpas-container.sh
+++ build-mpas-container.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #LA-UR-19-27938
+#Download charliecloud
+cd ~
+git clone git@github.com:hpc/charliecloud.git
+#Build the openmpi-no-ucx first
+cd - #go back to MPAS_Container directory
+~/charliecloud/bin/ch-build -t openmpi-no-ucx -f Dockerfile.openmpi .
 #This calls ch-build and or grow.
 ~/charliecloud/bin/ch-build -t mpas-debian9 -f Dockerfile-debian9.mpas .
 #This creates a tarball and places it in the tmp directory.
